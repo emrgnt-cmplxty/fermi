7b57750b76a36b8a16512ca0d68da83cee3635f4
Owen Colegrove <owen@algofi.org>
2022-07-05 21:31:55 -0400

    hygeine

--- proc/src/account.rs
+++ proc/src/account.rs
@@ -1,20 +1,20 @@
 
 extern crate engine;
 
-use std::fmt::Debug;
 use std::collections::HashMap;
+use std::fmt::Debug;
+use std::time::SystemTime;
 use engine::orders;
 use engine::domain::{OrderSide};
 use engine::orderbook::{Orderbook, OrderProcessingResult, Success, Failed};
 use engine::orders::{OrderRequest};
 
-use std::time::SystemTime;
-
-
-// for storing local order data
-#[derive(Debug)]
+// TODO
+// 1.) Add support for market orders
+// 2.) should we consider ways to avoid passing full mut self in process_limit_result?
 
 /// Controller for account associated to a given market
+#[derive(Debug)]
 pub struct MarketController<Asset> 
 where
     Asset: Debug + Clone + Copy + Eq,
@@ -28,8 +28,7 @@ where
 
 /// Orderbook account
 #[derive(Debug)]
-pub struct Account
-{
+pub struct Account {
     pub n_orders: u64,
     pub account_id: u64,
     pub base_balance: f64,
@@ -38,8 +37,7 @@ pub struct Account
     pub quote_escrow: f64,
 }
 
-impl Account
-{
+impl Account {
     fn new(account_id: u64, base_balance: f64, quote_balance: f64) -> Self {
         Account{
             n_orders: 0, 
@@ -50,14 +48,13 @@ impl Account
             quote_escrow: 0.0 
         }
     }
-
 }
 
 #[derive(Debug)]
 pub enum AccountErrors {
     Creation(String),
     Lookup(String),
-    Order(String)
+    OrderProc(String)
 }
 
 impl<Asset> MarketController <Asset>
@@ -65,7 +62,7 @@ where
     Asset: Debug + Clone + Copy + Eq,
 {
     pub fn new(base_asset: Asset, quote_asset: Asset) -> Self {
-        MarketController {
+        MarketController{
             base_asset: base_asset,
             quote_asset: quote_asset,
             accounts: HashMap::new(),
@@ -73,20 +70,22 @@ where
         }
     }
     
-    fn init_account_order(account: &mut Account, side: OrderSide, price: f64, qty: f64) {
+    // modifying an account associated to a new order
+    fn proc_order_init(account: &mut Account, side: OrderSide, price: f64, qty: f64) {
         account.n_orders = account.n_orders + 1;
         if matches!(side, OrderSide::Ask) { 
-            // E.g. ask 1 BTC @ $20k requires 1 BTC (base) from balance to escrow
+            // E.g. ask 1 BTC @ $20k moves 1 BTC (base) from balance to escrow
             account.base_balance -= qty; 
             account.base_escrow += qty;  
         } else { 
-            // E.g. bid 1 BTC @ $20k nives 20k USD (quote) from balance to escrow
+            // E.g. bid 1 BTC @ $20k moves 20k USD (quote) from balance to escrow
             account.quote_balance -= qty * price; 
             account.quote_escrow += qty * price; 
-        };
+        }
     }
     
-    fn process_account_fill(account: &mut Account, side: OrderSide, price: f64, qty: f64, order_increment: i64) {
+    // modifying an account associated to a fill
+    fn proc_order_fill(account: &mut Account, side: OrderSide, price: f64, qty: f64, order_increment: i64) {
         account.n_orders = (account.n_orders as i64 + order_increment) as u64; 
         if matches!(side, OrderSide::Ask) { 
             // E.g. fill ask 1 BTC @ 20k adds 20k USD (quote) to bal, subtracts 1 BTC (base) from escrow
@@ -96,7 +95,7 @@ where
             // E.g. fill bid 1 BTC @ 20k adds 1 BTC (base) to bal, subtracts 20k USD (quote) from escrow
             account.base_balance += qty; 
             account.quote_escrow -= qty * price; 
-        };
+        }
     }
 
     // create account inside market controller
@@ -118,7 +117,7 @@ where
                 assert!(account.base_balance  > qty);
             } else { 
                 assert!(account.quote_balance  > qty * price);
-            };
+            }
         }
         let order: OrderRequest<Asset>= orders::new_limit_order_request(
             self.base_asset,
@@ -133,9 +132,6 @@ where
     }
 
     // process output 
-    // TODO - should we consider ways to avoid passing full mut self?
-    // Attempts to pass previously loaded account resulted in double borrow err....
-    // fn process_order_result(account: &mut Account, accounts: &mut HashMap<u64, Account>, res: OrderProcessingResult) {
     fn process_limit_result(&mut self, account_id: u64, sub_side: OrderSide, sub_price: f64, sub_qty: f64,  res: OrderProcessingResult) -> Result<(), AccountErrors> {
         for order in res {
             match order {
@@ -144,27 +140,27 @@ where
                     let account: &mut Account = self.accounts.get_mut(&account_id).unwrap();
                     self.order_to_account.insert(order_id, account_id);
                     // special case handling for initializing order
-                    MarketController::<Asset>::init_account_order(account, sub_side, sub_price, sub_qty);
+                    MarketController::<Asset>::proc_order_init(account, sub_side, sub_price, sub_qty);
                 },
                 // subsequent orders are expected to be an PartialFill or Fill results
                 Ok(Success::PartiallyFilled{order_id, side, price, qty, ..}) => {
                     let existing_id: &u64 = self.order_to_account.get(&order_id).unwrap();
                     let account: &mut Account = self.accounts.get_mut(&existing_id).unwrap();
-                    MarketController::<Asset>::process_account_fill(account, side, price, qty, 0);
+                    MarketController::<Asset>::proc_order_fill(account, side, price, qty, 0);
                     // over-write existing order
                     self.order_to_account.insert(order_id, account_id);
                 },
                 Ok(Success::Filled{order_id, side, price, qty, ..}) => {
                     let existing_id: &u64 = self.order_to_account.get(&order_id).unwrap();
                     let account: &mut Account = self.accounts.get_mut(&existing_id).unwrap();
-                    MarketController::<Asset>::process_account_fill(account, side, price, qty, -1);
+                    MarketController::<Asset>::proc_order_fill(account, side, price, qty, -1);
                     // erase existing order
                     self.order_to_account.remove(&order_id).unwrap();
                 }
                 Ok(Success::Amended { .. }) => { panic!("THIS NEEDS TO BE IMPLEMENTED") }
                 Ok(Success::Cancelled { .. }) => { panic!("THIS NEEDS TO BE IMPLEMENTED") }
                 Err(_) => { 
-                    return Err(AccountErrors::Order("AN UNEXPECTED ERROR WAS ENCOUNTERED".to_string()));
+                    return Err(AccountErrors::OrderProc("AN UNEXPECTED ERROR WAS ENCOUNTERED DURING ORDER PROCESSING".to_string()));
                 }
             }
         }
@@ -172,7 +168,7 @@ where
     }
 
     pub fn get_account(&self, account_id: u64) -> &Account {
-        let account = self.accounts.get(&account_id).unwrap();
+        let account: &Account = self.accounts.get(&account_id).unwrap();
         account
     }
 }

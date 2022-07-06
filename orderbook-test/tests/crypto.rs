e4f6c2f7047df3261c83e4c2526a0a8ec74012ed
Ubuntu <ubuntu@ip-172-31-28-86.us-east-2.compute.internal>
2022-07-06 17:28:34 +0000

    clean imports

--- orderbook-test/tests/crypto.rs
+++ orderbook-test/tests/crypto.rs
@@ -1,8 +1,3 @@
-#[macro_use]
-extern crate criterion;
-
-use criterion::Criterion;
-
 use diem_crypto_derive::{BCSCryptoHash, CryptoHasher};
 use rand::{prelude::ThreadRng, thread_rng};
 use serde::{Deserialize, Serialize};

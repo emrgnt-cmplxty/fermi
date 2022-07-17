dbfbda14bb91c7786edc0e946b3c1d0c557c8765
Owen Colegrove <owen@algofi.org>
2022-07-16 20:11:06 -0400

    add core tests, add block hash fcn

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,21 +1,32 @@
 use std::{fmt::Debug};
+
+use super::transaction::{TxnRequest, TxnVariant};
 use diem_crypto::{
     hash::{CryptoHash, HashValue},
 };
-
-use super::transaction::{TxnRequest};
+use types::{
+    spot::{TestDiemCrypto}
+};
 
 pub struct BlockContainer <Variant>
 where
-Variant: Debug + Clone + CryptoHash,
+    Variant: Debug + Clone + CryptoHash,
 {
     pub blocks: Vec<Block<Variant>>,
 }
 
 pub struct Block <Variant>
 where
-Variant: Debug + Clone + CryptoHash,
+    Variant: Debug + Clone + CryptoHash,
 {
     pub txns: Vec<TxnRequest<Variant>>,
     pub block_hash: HashValue
+}
+
+pub fn generate_block_hash(txns: &Vec<TxnRequest<TxnVariant>>) -> HashValue{
+    let mut hash_string = String::from("");
+    for txn in txns {
+        hash_string += &txn.txn.hash().to_string();
+    }
+    return TestDiemCrypto(hash_string).hash()
 }

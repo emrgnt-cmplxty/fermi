bcd2b17529c67351e3af10ee1d3d23bba4dc4e20
John Clarke <john@algofi.org>
2022-08-04 22:15:06 +0000

    added benchmark script and node package

---
+++ benchmark/benchmark/commands.py
@@ -0,0 +1,97 @@
+# Copyright(C) Facebook, Inc. and its affiliates.
+from os.path import join
+
+from benchmark.utils import PathMaker
+
+
+class CommandMaker:
+
+    @staticmethod
+    def cleanup():
+        return (
+            f'rm -r .db-* ; rm .*.json ; mkdir -p {PathMaker.results_path()}'
+        )
+
+    @staticmethod
+    def clean_logs():
+        return f'rm -r {PathMaker.logs_path()} ; mkdir -p {PathMaker.logs_path()}'
+
+    @staticmethod
+    def compile(mem_profiling):
+        if mem_profiling:
+            params = ["--profile", "bench-profiling", "--features", "benchmark dhat-heap"]
+        else:
+            params = ["--release", "--features", "benchmark"]
+        return ["cargo", "build"] + params
+
+    @staticmethod
+    def generate_key(filename):
+        assert isinstance(filename, str)
+        return f'./node generate_keys --filename {filename}'
+
+    @staticmethod
+    def run_primary(keys, committee, store, parameters, execution, debug=False):
+        assert isinstance(keys, str)
+        assert isinstance(committee, str)
+        assert isinstance(parameters, str)
+        assert isinstance(execution, str)
+        assert isinstance(debug, bool)
+        v = '-vvv' if debug else '-vv'
+        return (f'./node {v} run --keys {keys} --committee {committee} '
+                f'--store {store} --parameters {parameters} --execution {execution} primary')
+
+    @staticmethod
+    def run_no_consensus_primary(keys, committee, store, parameters, debug=False):
+        assert isinstance(keys, str)
+        assert isinstance(committee, str)
+        assert isinstance(parameters, str)
+        assert isinstance(debug, bool)
+        v = '-vvv' if debug else '-vv'
+        return (f'./node {v} run --keys {keys} --committee {committee} '
+                f'--store {store} --parameters {parameters} primary --consensus-disabled')
+
+    @staticmethod
+    def run_worker(keys, committee, store, parameters, execution, id, debug=False):
+        assert isinstance(keys, str)
+        assert isinstance(committee, str)
+        assert isinstance(parameters, str)
+        assert isinstance(execution, str)
+        assert isinstance(debug, bool)
+        v = '-vvv' if debug else '-vv'
+        return (f'./node {v} run --keys {keys} --committee {committee} '
+                f'--store {store} --parameters {parameters} --execution {execution} worker --id {id}')
+
+    @staticmethod
+    def run_client(address, size, rate, execution, nodes):
+        assert isinstance(address, str)
+        assert isinstance(size, int) and size > 0
+        assert isinstance(rate, int) and rate >= 0
+        assert isinstance(execution, str)
+        assert isinstance(nodes, list)
+        assert all(isinstance(x, str) for x in nodes)
+        nodes = f'--nodes {" ".join(nodes)}' if nodes else ''
+        return f'./benchmark_client {address} --size {size} --rate {rate} --execution {execution} {nodes}'
+
+    @staticmethod
+    def alias_demo_binaries(origin):
+        assert isinstance(origin, str)
+        client = join(origin, 'demo_client')
+        return f'rm demo_client ; ln -s {client} .'
+
+    @staticmethod
+    def run_demo_client(keys, ports):
+        assert all(isinstance(x, str) for x in keys)
+        assert all(isinstance(x, int) and x > 1024 for x in ports)
+        keys_string = ",".join(keys)
+        ports_string = ",".join([str(x) for x in ports])
+        return f'./demo_client run --keys "{keys_string}" --ports "{ports_string}"'
+
+    @staticmethod
+    def kill():
+        return 'tmux kill-server'
+
+    @staticmethod
+    def alias_binaries(origin):
+        assert isinstance(origin, str)
+        node, client = join(origin, 'node'), join(origin, 'benchmark_client')
+        return f'rm node ; rm benchmark_client ; ln -s {node} . ; ln -s {client} .'

db38344931e65b4de24be8984cb88f7e43dfad98
Armstrong Park <goarmstrongpark.ap@gmail.com>
2022-08-22 16:00:24 -0400

    relay -> relayer folder

---
+++ core/src/relayer/server.rs
@@ -0,0 +1,26 @@
+use crate::validator::state::ValidatorState;
+use gdex_types::proto::{Relayer, RelayerRequest, RelayerResponse};
+use std::sync::Arc;
+use tonic::{Request, Response, Status};
+
+pub struct RelayerService {
+    pub state: Arc<ValidatorState>,
+}
+
+#[tonic::async_trait]
+impl Relayer for RelayerService {
+    async fn read_latest_block_info(
+        &self,
+        request: Request<RelayerRequest>,
+    ) -> Result<Response<RelayerResponse>, Status> {
+        let validator_state = &self.state;
+        validator_state.validator_store.prune();
+        println!("Returned succesfully!");
+
+        // We can now return true because errors will have been caught above
+        let reply = RelayerResponse { successful: true };
+
+        // Sending back a response
+        Ok(Response::new(reply))
+    }
+}

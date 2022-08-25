8b4fcbe2e9b1a2bd5ce7cda097d3ff05637cfe4b
Paul-Colella <paul.colella.a@gmail.com>
2022-08-25 23:36:46 +0000

    fix tests

--- core/src/validator/state.rs
+++ core/src/validator/state.rs
@@ -329,6 +329,7 @@ mod test_validator_state {
     #[tokio::test]
     pub async fn single_node_init() {
         let master_controller = MasterController::default();
+        master_controller.initialize_controllers();
 
         let key: ValidatorKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         let public_key = ValidatorPubKeyBytes::from(key.public());
@@ -364,6 +365,7 @@ mod test_validator_state {
 
     fn create_test_validator() -> ValidatorState {
         let master_controller = MasterController::default();
+        master_controller.initialize_controllers();
 
         let key: ValidatorKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
         let public_key = ValidatorPubKeyBytes::from(key.public());

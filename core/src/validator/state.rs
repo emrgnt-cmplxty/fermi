a92af45f33a75669cd57b62e8c58bc3ee6f19330
Owen Colegrove <owen@algofi.org>
2022-08-31 16:41:42 -0400

    remove premature unwrap

--- core/src/validator/state.rs
+++ core/src/validator/state.rs
@@ -272,18 +272,22 @@ impl ExecutionState for ValidatorState {
             .insert_confirmed_transaction(transaction, consensus_output);
 
         // handle transactions for bank controller
-        self.master_controller
+        self
+            .master_controller
             .bank_controller
             .lock()
             .unwrap()
-            .handle_consensus_transaction(transaction)
-            .unwrap();
-        self.master_controller
+            .handle_consensus_transaction(transaction)?;
+
+
+        // handle transactions for spot controller
+        self
+            .master_controller
             .spot_controller
             .lock()
             .unwrap()
-            .handle_consensus_transaction(transaction)
-            .unwrap();
+            .handle_consensus_transaction(transaction)?;
+
 
         Ok((consensus_output.clone(), execution_indices))
     }

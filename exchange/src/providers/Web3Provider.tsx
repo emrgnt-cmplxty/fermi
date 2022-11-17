595a850e8bd1f14973173099021a770fc3626136
Owen Colegrove <owen@algofi.org>
2022-11-17 14:20:40 -0500

    modify web3 provider to default to deployer

--- exchange/src/providers/Web3Provider.tsx
+++ exchange/src/providers/Web3Provider.tsx
@@ -1,9 +1,7 @@
 import { Web3Context } from 'hooks/useWeb3Context'
 import { ReactElement, useCallback, useEffect, useState } from 'react'
 import { FermiAccount, FermiUtils, FermiTypes } from 'fermi-js-sdk'
-
-
-
+import config from '../../../configs/protonet.json';
 
 export type Web3Data = {
   connectWallet: (account: FermiAccount | undefined) => Promise<void>
@@ -47,10 +45,18 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({
     async (account: FermiAccount.FermiAccount | undefined) => {
       setLoading(true)
       try {
+        const authorities = Object.keys(config['authorities']);
+        // @ts-ignore
+        const deploymentAuthority = config['authorities'][authorities[0]];
+        console.log("authorities=", authorities)
+        // uncomment this later when we are ready for arbitrary users
+        // if (account === undefined) {
+        //   account = await FermiAccount.generateAccount()
+        //   localStorage.setItem('axionPrivateKeyHex', FermiUtils.bytesToHex(account.privateKey))
+        //   localStorage.setItem('axionPublicKeyHex', FermiUtils.bytesToHex(account.publicKey))
+        // }
         if (account === undefined) {
-          account = await FermiAccount.generateAccount()
-          localStorage.setItem('axionPrivateKeyHex', FermiUtils.bytesToHex(account.privateKey))
-          localStorage.setItem('axionPublicKeyHex', FermiUtils.bytesToHex(account.publicKey))
+
         }
         setCurrentAccount(account.publicAddress)
         setPublicKey(account.privateKey)
@@ -58,7 +64,8 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({
         setConnected(true)
         setLoading(false)
       } catch (e) {
-        setError(e)
+        console.log("err=", e);
+        // setError(e)
         disconnectWallet();
         setLoading(false)
       }
@@ -66,22 +73,43 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({
     [],
   )
 
+  const setWalletInfo = async (deploymentAuthority: any) => {
+    // @ts-ignore
+    const deployerPrivateKey = FermiUtils.hexToBytes(
+      deploymentAuthority.private_key
+    );
+    const deployerPublicKey = await FermiAccount.getPublicKey(deployerPrivateKey);
+      const account =  {
+        privateKey: deployerPrivateKey,
+        publicKey: deployerPublicKey,
+        publicAddress: FermiUtils.bytesToHex(deployerPublicKey),
+      } as FermiAccount.FermiAccount
+      connectWallet(account).catch((e) => {throw Error(e)})
+      console.log("account=", account)
+  }
+
     // handle logic to eagerly connect to the injected ethereum provider,
   // if it exists and has granted access already
   useEffect(() => {
-    const axionPrivateKeyHex = localStorage.getItem('axionPrivateKeyHex')
-    const axionPublicKeyHex = localStorage.getItem('axionPublicKeyHex')
-    console.log("axionPrivateKeyHex=", axionPrivateKeyHex)
-    console.log("axionPublicKeyHex=", axionPublicKeyHex)
+    const authorities = Object.keys(config['authorities']);
+    console.log("authorities=", authorities)
+    // @ts-ignore
+    const deploymentAuthority = config['authorities'][authorities[0]];
+    setWalletInfo(deploymentAuthority);
+    // uncomment this later when we are reaady for arbitrary users
+    // const axionPrivateKeyHex = localStorage.getItem('axionPrivateKeyHex')
+    // const axionPublicKeyHex = localStorage.getItem('axionPublicKeyHex')
+    // console.log("axionPrivateKeyHex=", axionPrivateKeyHex)
+    // console.log("axionPublicKeyHex=", axionPublicKeyHex)
     
-    if (axionPrivateKeyHex && axionPublicKeyHex) {
-      const account =  {
-        privateKey: FermiUtils.hexToBytes(axionPrivateKeyHex),
-        publicKey: FermiUtils.hexToBytes(axionPublicKeyHex),
-        publicAddress: axionPublicKeyHex
-      } as FermiAccount.FermiAccount
-      connectWallet(account).catch((e) => {throw Error(e)})
-    }
+    // if (axionPrivateKeyHex && axionPublicKeyHex) {
+    //   const account =  {
+    //     privateKey: FermiUtils.hexToBytes(axionPrivateKeyHex),
+    //     publicKey: FermiUtils.hexToBytes(axionPublicKeyHex),
+    //     publicAddress: axionPublicKeyHex
+    //   } as FermiAccount.FermiAccount
+    //   connectWallet(account).catch((e) => {throw Error(e)})
+    // }
   }, [])
 
   // Tx methods
